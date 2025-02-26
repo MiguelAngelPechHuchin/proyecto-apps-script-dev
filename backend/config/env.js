cf5c43cba3777a3de1e8e4fe08a16d3d78751da6
1234 <Miguel Pech@MIGUELPECH.Hola.com>
2025-02-25 23:02:18 -0600

    Commit o cualquier nombre

---
+++ backend/config/env.js
@@ -0,0 +1,6 @@
+function env_() {
+    return {
+      ID_DATABASE: "1TjGmtwlGgYBrx_PQEBxp3uu70UewTU6iSAu4YS-Dy_Y",
+      SH_REGISTRO_USUARIOS: "Registro usuarios",
+    };
+  }  
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,6 @@ const doGet = () => HtmlService.createTemplateFromFile("frontend/Index")
     'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"'
 ) 
 .setTitle("proyecto_apps_scrip_dev")
-.setFaviconUrl("https://cdn-icons-png.flaticon.com/128/4615/4615513.png");
+.setFaviconUrl("https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png");
 
 const include = (ruta) => HtmlService.createHtmlOutputFromFile(ruta).getContent();
